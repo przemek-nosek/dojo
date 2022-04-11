80a1bc9ae6c5ba6d0877a12a69b4aee9769efe3a
Przemek Nosek <przemek.nosek@o2.pl>
2022-04-11 09:09:51 +0200

    added cross origin

--- src/main/java/com/example/demo/controller/JokeController.java
+++ src/main/java/com/example/demo/controller/JokeController.java
@@ -4,6 +4,7 @@ import com.example.demo.model.Joke;
 import com.example.demo.service.JokeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class JokeController {
     private final JokeService jokeService;
 
     @GetMapping
+    @CrossOrigin(origins = "http://localhost:4200")
     public ResponseEntity<Joke> getRandomJoke() {
         Joke joke = jokeService.getRandomJoke();
 
